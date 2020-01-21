c02bdf2234b093bdc8fff9a033cf7e33632de4fb
Fatimah <daviesfatimah97@gmail.com>
2020-01-21 12:39:29 -0800

    Updates

---
+++ precache-manifest.b2112ea05e2a630df3960d2ad4fa875a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1583b9eb133357587c10d8e4c2abe9af",
+    "url": "/react-applications/index.html"
+  },
+  {
+    "revision": "3e27ea0aa7eb8c6ac82b",
+    "url": "/react-applications/static/css/2.db15d63e.chunk.css"
+  },
+  {
+    "revision": "c03ff083b5446944a42f",
+    "url": "/react-applications/static/css/main.3bff0630.chunk.css"
+  },
+  {
+    "revision": "3e27ea0aa7eb8c6ac82b",
+    "url": "/react-applications/static/js/2.cda5968c.chunk.js"
+  },
+  {
+    "revision": "c03ff083b5446944a42f",
+    "url": "/react-applications/static/js/main.6caff4ab.chunk.js"
+  },
+  {
+    "revision": "601eaddd5511224a6f54",
+    "url": "/react-applications/static/js/runtime~main.3792a09d.js"
+  },
+  {
+    "revision": "94032adee1f104ba22a1ba1aebcc7d12",
+    "url": "/react-applications/static/media/bata1.94032ade.jpg"
+  },
+  {
+    "revision": "6cec9f3f265403e21d59d1faef03b2bc",
+    "url": "/react-applications/static/media/bata2.6cec9f3f.jpg"
+  },
+  {
+    "revision": "a0530f5a2d2a5491fb6df2c7aa3c4934",
+    "url": "/react-applications/static/media/bata4.a0530f5a.jpg"
+  },
+  {
+    "revision": "cf9d1f8f1148f1565ff0bd11878207f1",
+    "url": "/react-applications/static/media/bata5.cf9d1f8f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-applications/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-applications/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-applications/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-applications/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-applications/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "99ea6929d7f2f220d915649d4f6d869f",
+    "url": "/react-applications/static/media/profpic.99ea6929.png"
+  }
+]);
